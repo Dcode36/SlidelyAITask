44d7966644db1fca696f1db546712c27be968bc1
Digvijay Kadam <92368983+Dcode36@users.noreply.github.com>
2024-06-13 13:29:54 +0000

    Add message box to Form1

---
+++ Form1.vb
@@ -0,0 +1,9 @@
+Public Class Form1
+    Public Sub New()
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        MsgBox("John Doe here, this is my Slidely AI task")
+    End Sub
+End Class
